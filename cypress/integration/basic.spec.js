5c00b53e70fa4a23ccebb98405afbaccb720a3fa
Raniere de Souza Santos <ranieresantos@ntopus.com.br>
2022-05-11 04:15:55 -0300

    First running Cypress test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,4 +1,10 @@
+/// <reference types="cypress" />
+
 describe('Basic test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8085')
+  })
+
   it('should have a <h1> with the text "Hello RPG Grid!"', () => {
     cy.get('h1').should('contain', 'Hello RPG Grid!')
   })
