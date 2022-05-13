9c6491439ae42f72b3f19efa74b494265cc88141
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2022-05-13 03:00:56 -0300

    Adding animation to the page title (no need to change the tests)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './styles/global.css'
 
 document.querySelector('#app').innerHTML = `
+  <style>
+    h1 {
+      animation: fade-in 1.5s linear;
+    }
+  </style>
   <h1 data-testid="home-title">Hello, adventurer!</h1>
 `
