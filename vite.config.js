c83c445959649ec68e9f06dbf23df88676131d01
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-04-27 06:31:34 -0300

    Upgrading Vite from v2.x to v4.x; Making some configurations and scripts better; CHANGELOG (Unreleased)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(() => {
-  const env = loadEnv('test', process.cwd(), '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     preview: {
