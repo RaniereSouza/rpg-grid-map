23bf072711e2154d89d1b7c45b1709d4c396943f
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-05-02 04:30:34 -0300

    Unit testing with more verbose output

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,9 @@ import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.spec.js'],
-    exclude: [...configDefaults.exclude, 'drafts'],
-    watch:   false,
+    include:   ['src/**/*.spec.js'],
+    exclude:   [...configDefaults.exclude, 'drafts'],
+    watch:     false,
+    reporters: ['verbose'],
   },
 })
